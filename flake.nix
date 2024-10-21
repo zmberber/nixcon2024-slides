a26e21ff86430c7b76b9bebdad616b95e9e8c1c6
Zeremonienmeister Berber <git@zmberber.com>
2024-10-21 16:00:37 +0200

    begin

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixCon 2024 Slides Environment";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    ...
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems ( system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      {
+        default = import ./shell.nix { inherit pkgs };
+      }
+    );
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }: pkgs.mkShell {
+  name = "Muller-Satterthwaite--Arrow Correspondence LaTeX shell";
+  packages = [
+    pkgs.texliveFull
+  ];
+  shellHook = ''
+    echo "This is the NixCon 2024 Slides Environment LaTeX shell!"
+  '';
+};
